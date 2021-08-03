05a3e70ce849ef73634852a4466692c758c5c402
alex <a.emchenko95@gmail.com>
2021-08-03 21:37:23 +0300

    вызвал метод

---
+++ app/user/route.php
@@ -0,0 +1,29 @@
+<?php
+
+class Route
+{
+
+    public function run()
+    {
+        $server = explode('/', $_SERVER['REQUEST_URI']);
+
+        $module = $server[1];
+        $controller_name = $server[2];
+        $action_name = $server[3];
+
+        $controller_file = ($controller_name) . '.php';
+        $controller_path = "app/user/controller/" . $controller_file;
+        if (file_exists($controller_path)) {
+            include $controller_path;
+        } else {
+            include 'app/404.php';
+        }
+        $controller = new $controller_name;
+        $action = $action_name;
+        if (method_exists($controller, $action)) {
+            $controller->$action();
+        } else {
+            echo 'нет такого метода';
+        }
+    }
+}
--- index.php
+++ index.php
@@ -1,23 +1,7 @@
 <?php
-var_dump((explode( '/', $_SERVER['REQUEST_URI'] )));
-$server = explode( '/', $_SERVER['REQUEST_URI'] );
+include 'app/user/route.php';
 
-$module = $server[1];
-$controller_name = $server[2];
-$action_name = $server[3];
+$run = new Route();
+$run->run();
 
-var_dump($module.'/'.$controller_name.'/'.$action_name);
-$controller_file = ($controller_name).'.php';
-$controller_path = "app/user/controller/".$controller_file;
-if (file_exists($controller_path)){
-    include $controller_path;
-}else{
-    include 'app/404.php';
-}
-$controller = new $controller_name;
-$action = $action_name;
-if (method_exists($controller,$action)){
-    $controller->$action();
-}else{
-    echo 'нет такого метода';
-}
+
