dc008f3e7d0d243033313a799e087432b8becbc1
alex <a.emchenko95@gmail.com>
2021-08-04 00:10:47 +0300

    правлю роут

--- app/user/route.php
+++ app/user/route.php
@@ -6,12 +6,13 @@ class Route
     {
         $server = explode('/', $_SERVER['REQUEST_URI']);
         if ($server['1'] === 'store') {
+
             $module = $server[1];
             $controller_name = $server[2];
             $action_name = $server[3];
             $controller_file = ($controller_name) . '.php';
             $controller_path = "app/store/controller/" . $controller_file;
-            var_dump($_GET);
+            var_dump($server);
 
         } elseif (count($server) == 3) {
             $controller_name = $server[1];
